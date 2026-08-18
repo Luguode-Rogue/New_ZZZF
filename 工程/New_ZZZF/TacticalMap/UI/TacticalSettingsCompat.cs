853416f1292c66f90ffc158f2c7b2a9a021b328d
Luguode-Rogue <66106855+Luguode-Rogue@users.noreply.github.com>
2026-08-18 12:02:11 +0800

    fix: remove ambiguous TacticalSettings UI proxy

--- 工程/New_ZZZF/TacticalMap/UI/TacticalSettingsCompat.cs
+++
@@ -1,9 +0,0 @@
-using ConfigTacticalSettings = New_ZZZF.TacticalMap.Config.TacticalSettings;
-
-namespace New_ZZZF.TacticalMap.UI
-{
-    internal static class TacticalSettings
-    {
-        public static ConfigTacticalSettings Instance => ConfigTacticalSettings.Instance;
-    }
-}
