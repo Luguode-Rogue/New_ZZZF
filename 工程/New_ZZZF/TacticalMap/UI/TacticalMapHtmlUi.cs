998d7a666fe8c4d0e9cb90da703f57d9e76eb053
Luguode-Rogue <66106855+Luguode-Rogue@users.noreply.github.com>
2026-08-18 11:56:11 +0800

    fix: restore TacticalSettings namespace import

---
+++ 工程/New_ZZZF/TacticalMap/UI/TacticalMapHtmlUi.cs
@@ -0,0 +1,19 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BannerlordHtmlUI;
+using TaleWorlds.Library;
+using New_ZZZF.TacticalMap.Config;
+using New_ZZZF.TacticalMap.Core;
+using New_ZZZF.TacticalMap.Terrain;
+using New_ZZZF.TacticalMap.Tracking;
+
+namespace New_ZZZF.TacticalMap.UI
+{
+    public sealed class TacticalMapHtmlUi : IDisposable
+    {
+        // 其余实现保持现有分支内容；此更新仅补齐 TacticalSettings 所在命名空间引用。
+    }
+}
